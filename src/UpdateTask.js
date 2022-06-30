c6c0957a5f0fec0e5f9a2425d72ea9621a626c11
Tatsiana <gtaneka@gmail.com>
2022-06-30 11:02:36 -0400

    update is working

--- src/UpdateTask.js
+++ src/UpdateTask.js
@@ -3,33 +3,31 @@ import {connect} from "react-redux";
 
 
 
-function UpdateTodo(props) {
+function UpdateTask(props) {
     console.log(props)
     const [newTask, setNewTask] = useState(props.grandTask.task);
 
     function saveButton() {
-        const updatedCard = {
-            task: newTask
-        }
-        props.updateTask(props.grandTask, updatedCard)
+        props.updateTask(props.grandTask.id, newTask)
         props.toggle()
     }
+
 
     return (
         <div>
             <input value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
             <button onClick={saveButton}>Save</button>
             <button onClick={props.toggle}>Cancel</button>
+        <hr/>
         </div>
     )
 }
 
-    const mapDispatchToProps = dispatch => ({
-        updateTask: (id, updatedCard) => dispatch ({
-            type: 'UPDATE_TASK',
-            payload: {id, updatedCard}
-        })
-    })
+const mapDispatchToProps = dispatch => ({
+    updateTask: (id, task) => dispatch({
+        type: 'UPDATE_TASK',
+        payload: {id, task}})
+})
 
 
-export default connect(null, mapDispatchToProps)(UpdateTodo);
+export default connect(null, mapDispatchToProps)(UpdateTask);
