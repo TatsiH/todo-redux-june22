7f1a599a7968d0ee208ce2a54ed5ec49e9e5fd42
Tatsiana <gtaneka@gmail.com>
2022-06-30 10:34:12 -0400

    update button

---
+++ src/DeleteTask.js
@@ -0,0 +1,17 @@
+import {connect} from "react-redux";
+
+function DeleteTask (props) {
+    return(
+        <div>
+            <button onClick={() => props.deleteTask(props.grandTask.id)}>Del</button>
+        </div>
+    )
+}
+const mapDispatchToProps = dispatch => ({
+    deleteTask: (id) => dispatch({
+        type: "DELETE_TASK",
+        payload: {id}
+    })
+})
+
+export default connect(null, mapDispatchToProps)(DeleteTask);
